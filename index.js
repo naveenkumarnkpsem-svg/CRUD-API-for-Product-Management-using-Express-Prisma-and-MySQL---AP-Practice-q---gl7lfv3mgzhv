08c649cd244656a2c3974bcbe0bb3931f9ffe636
naveenkumarnkpsem-svg <naveenkumarnkpsem@gmail.com>
2025-11-24 14:09:04 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const productRoutes = require('./routes/productRoutes');
+
+const app = express();
+app.use(express.json());
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+module.exports=app;
